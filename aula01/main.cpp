ea4d2e433859fe6d981685cafda9c38cceae3d73
Regimar <regimar.negreiros@gmail.com>
2025-08-26 10:51:27 -0300

    Aula 01
    Terminal

---
+++ aula01/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello world!" << endl << endl;
+
+    string nome;
+    cout << "Digite seu nome: ";
+    cin >> nome;
+
+    cout << endl << "Seu nome é: " << nome << endl;
+    return 0;
+}
