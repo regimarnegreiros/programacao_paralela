1cbe394f5f0eddfd04bb8a58245f11e64ffd70e7
Regimar <regimar.negreiros@gmail.com>
2025-10-01 18:40:00 -0300

    Tarefa 2 - ex01

--- tarefa02/ex01.cpp
+++ tarefa02/ex01.cpp
@@ -9,13 +9,14 @@ c) Configure o programa para rodar com 4 threads. */
 using namespace std;
 
 int main() {
-    omp_set_num_threads(4);
+    omp_set_num_threads(4); // Criação de 4 threads
 
     #pragma omp parallel
     {
-        int thread_id = omp_get_thread_num();
-        int total_threads = omp_get_num_threads();
+        int thread_id = omp_get_thread_num(); // Coleta do id de cada thread
+        int total_threads = omp_get_num_threads(); // Total de threads
 
+        // As 4 threads imprimem seu id em paralelo
         cout << "Thread " << thread_id << " de " << total_threads << " threads." << endl;
     }
     return 0;
