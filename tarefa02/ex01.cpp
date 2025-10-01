16194107dceaad763bb53513e1beba36e29bf579
Regimar <regimar.negreiros@gmail.com>
2025-10-01 18:37:38 -0300

    Tarefa 2 - ex01

--- tarefa02/ex01.cpp
+++ tarefa02/ex01.cpp
@@ -2,3 +2,21 @@
 a) Crie uma região paralela com #pragma omp parallel.
 b) Cada thread deve imprimir uma mensagem indicando seu número (omp_get_thread_num()) e o total de threads (omp_get_num_threads()).
 c) Configure o programa para rodar com 4 threads. */
+
+#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+int main() {
+    omp_set_num_threads(4);
+
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        int total_threads = omp_get_num_threads();
+
+        cout << "Thread " << thread_id << " de " << total_threads << " threads." << endl;
+    }
+    return 0;
+}
